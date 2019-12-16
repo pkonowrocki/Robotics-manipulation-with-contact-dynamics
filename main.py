3719867e015f026a22f6f930c30391e1272b955c
pkonowrocki <piotrek.konowrocki@gmail.com>
2019-12-16 16:12:59 +0100

    Debug purposes

--- main.py
+++ main.py
@@ -28,15 +28,15 @@ memorySize = args.memorySize
 numEpisodes = args.numEpisodes
 updatesPerStep = args.updatesPerStep
 
-if not os.path.exists(f"./models"):
-    os.mkdir(f"./models")
+if not os.path.exists(f'./models'):
+    os.mkdir(f'./models')
     print("Created models")
-if not os.path.exists(f"./models/{run}_h{hiddenSize}_b{batchSize}"):
-    os.mkdir(f"./models/{run}_h{hiddenSize}_b{batchSize}")
-    print(f"Created models/{run}_h{hiddenSize}_b{batchSize}")
-
+if not os.path.exists(f'./models/{run}_h{hiddenSize}_b{batchSize}'):
+    os.mkdir(f'./models/{run}_h{hiddenSize}_b{batchSize}')
+    print(f'Created models/{run}_h{hiddenSize}_b{batchSize}')
+print("Folder ready")
 if torch.cuda.is_available():
-    device = torch.device('cuda')
+    device = torch.device('cuda:0')
     print('cuda')
 else:
     device = torch.device('cpu')
@@ -44,40 +44,50 @@ else:
 
 memory = Memory(memorySize)
 env = gym.make('FetchSlide-v1')
+print("Env created")
+state = env.reset()
+print(f'Env tested: {state}')
 agent = NAF(gamma, tau, hiddenSize, env.observation_space["observation"].shape[0] + 3, env.action_space, device)
+print("Agent created")
 totalNumSteps = 0
 updates = 0
 rewards = []
 
 for episode in range(numEpisodes):
     state = env.reset()
-
-    state = stateToTensor(state).to(device=device)
+    print(f'Episode {episode}')
+    state = stateToTensor(state).to(device)
+    print(f'State to tensor')
     episodeReward = 0
     valueLossEp = 0
     updatesEpisode = 1
     while True:
+        print(f'Select action')
         action = agent.selectAction(state, None, None)
+        print(f'Selected: {action.cpu().numpy()[0]}')
+
         nextState, reward, done, _ = env.step(action.cpu().numpy()[0])
+        print('Step taken')
         totalNumSteps += 1
         episodeReward += reward
 
-        action = torch.Tensor(action).to(device=device)
-        mask = torch.Tensor([not done]).to(device=device)
-        nextState = stateToTensor(nextState).to(device=device)
-        reward = torch.Tensor([reward]).to(device=device)
-
-        memory.push(state, action, mask, nextState, reward)
-
-        state = nextState
+        action =action.cpu().numpy()
+        mask = torch.Tensor([not done]).cpu().numpy()
+        nextState = stateToTensor(nextState)
+        nextStateNumpy = nextState.cpu().numpy()
+        reward = torch.Tensor([reward]).cpu().numpy()
+        print('Push to memory')
+        memory.push(state, action, mask, nextStateNumpy, reward)
+        print('Pushed')
+        state = nextState.to(device)
 
         if len(memory) > batchSize:
-            
+            print('Training started')
             for _ in range(updatesPerStep):
                 transition = memory.sample(batchSize)
                 batch = Transition(*zip(*transition))
                 updatesEpisode += 1
-                valueLoss, policyLoss = agent.updateParameters(batch)
+                valueLoss, policyLoss = agent.updateParameters(batch, device)
                 valueLossEp += valueLoss
                 updates += 1
         
@@ -92,10 +102,10 @@ for episode in range(numEpisodes):
         episodeReward = 0
         while True:
             # env.render()
-            action = agent.selectAction(state).to(device=device)
-            nextState, reward, done, _ = env.step(action.numpy()[0])
+            action = agent.selectAction(state)
+            nextState, reward, done, _ = env.step(action.cpu().numpy()[0])
             episodeReward += reward
-            nextState = stateToTensor(nextState)
+            nextState = stateToTensor(nextState).to(device)
             state = nextState
             if done:
                 break
