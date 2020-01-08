14be748f676019edc9419134144edd76db531a59
Piotrek Konowrocki <piotrek.konowrocki@gmail.com>
2020-01-08 18:41:58 +0100

    fix

--- utils.py
+++ utils.py
@@ -5,5 +5,5 @@ def stateToTensor(state, goal):
     return torch.Tensor([np.concatenate((state["observation"], goal, state["achieved_goal"]))])
 
 def calcReward(state, goal, orginalDistance):
-    currentDistance = np.linalg.norm(desiredGoal - state)
+    currentDistance = np.linalg.norm(goal - state)
     return np.array([-np.expm1(currentDistance/orginalDistance)])
